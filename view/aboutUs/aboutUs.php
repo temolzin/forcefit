fee621c993bfda0cdbe0e3f5f378d880ac9f1b0d
bruno1026 <brunodeveloper26@gmail.com>
2023-04-08 11:24:56 -0600

    SE CORRIGIO EL MODULO COMPLETO DEL ABOUT US

--- view/aboutUs/aboutUs.php
+++ view/aboutUs/aboutUs.php
@@ -10,63 +10,36 @@ $menu->header('dashboard');
         <div class="">
             <div class="clearfix"></div>
             <div class="row">
-                <div class="col-md-12 col-sm-12  ">
-                    <div class="x_panel">
-
-                        <div class="about-section">
+                <div class="x_panel">
+                    <div class="about-section">
+                        <img class="img" src="public/img/forcefit.png" alt="">
+                        <div class="inner-container">
                             <h1>Acerca de Nosotros</h1>
-                        </div>
-
-                        <div class="about-section-2 background">
-                            <div class="parrafo">
-                                <h1 class="text center">¿Quiénes Somos?</h1>
-                                <p>Root Heim Company es una empresa especializada en el desarrollo de software de alta
-                                    calidad.
-                                    Nuestro equipo está compuesto por expertos en programación y tecnología que trabajan
-                                    en
-                                    conjunto para brindar soluciones innovadoras y personalizadas a
-                                    cada uno de nuestros clientes.</p>
-                            </div>
-                        </div>
-
-                        <div class="about-section-3 background">
-                            <div class="col-izq">
-                                <h1>Misión</h1>
-                                <p>Brindar servicios profesionales a nuestros clientes con soluciones agiles y
-                                    consistentes,
-                                    implementando las tecnologías actuales del mercado,
-                                    además de innovaciones que el mercado requiere; manteniendo siempre la calidad de
-                                    nuestro servicio, caracterizándonos por el trato, oportunidades y forma de trabajo
-                                    con nuestros colaboradores.</p>
-                            </div>
-                            <div class="col-med">
-                                <h1>Visión</h1>
-                                <p>Ser una empresa reconocida a nivel mundial ofreciendo servicios profesionales
-                                    especializados en tecnología y capacitación a empresas públicas y
-                                    privadas, derivado de importaciones y exportaciones a clientes nacionales y
-                                    extranjeros.</p>
-                            </div>
-                            <div class="col-izq">
-                                <h1>Filosofia</h1>
-                                <p>La filosofía de ROOT HEIM COMPANY S.A. de C.V., radica en ofrecer servicios
-                                    ampliamente profesionales dentro del mercado de las TIC´s
-                                    internacionalmente, con el propósito de mejorar e innovar constantemente, mostrando
-                                    la mejor presentación, calidad; afrontando los retos que
-                                    presentan las organizaciones hoy en dia.</p>
-                            </div>
-                        </div>
-
-                        <div class="about-section-4">
-
+                            <p class="text">
+                                En nuestro sistema de gestión de gimnasios, nos dedicamos a ayudar a los dueños de
+                                gimnasios y centros deportivos a optimizar y mejorar su gestión empresarial. Nuestro
+                                equipo está formado por expertos en tecnología y en el sector del fitness, y nos
+                                enorgullecemos de ofrecer una solución integral y personalizada que se adapta a las
+                                necesidades específicas de cada cliente.
+                            </p>
+                            <p class="text">
+                                Nuestro sistema de gestión de gimnasios cuenta con funciones avanzadas y herramientas de
+                                análisis que permiten a los propietarios y administradores tomar decisiones informadas
+                                basadas en datos en tiempo real, lo que les permite mejorar la eficiencia y el
+                                rendimiento de su negocio. Además, nuestro sistema está diseñado para ser fácil de usar
+                                y se puede personalizar para satisfacer las necesidades de cualquier tamaño de negocio.
+                            </p>
+                            <p class="text">
+                                En resumen, nuestro sistema de gestión de gimnasios es la solución ideal para aquellos
+                                que buscan mejorar la gestión y la rentabilidad de su negocio.
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </body>
 <?php
 $menu->footer();
 ?>
-
