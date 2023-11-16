2132081391238e1bb14e14bc3fdef975cf9a8196
Carmenlopez1720 <133041024+Carmenlopez1720@users.noreply.github.com>
2023-11-15 22:53:50 -0600

    FFIT-31 variables con .env

--- config/phinx.php
+++ config/phinx.php
@@ -15,13 +15,14 @@ return
         'default_migration_table' => 'migrations',
         'default_environment' => 'default',
         'default' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
+            "adapter" => "mysql",
+            "host" => $env['HOST'],
+            "name" => $env['DB'],
+            "user" => $env['USER'],
+            "pass" => $env['PASSWORD'],
+            "port" => $env['PORT'],
+            "charset" => $env['CHARSET'],
+            
         ],
     ],
     'version_order' => 'creation'
